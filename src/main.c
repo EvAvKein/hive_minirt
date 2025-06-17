cac9860d61d528ea735854bc5e529edd39945e83
Eve Aviv Keinan <evavkein@gmail.com>
2025-06-17 10:24:54 +0300

    get_data() initialization and doc comment

--- src/main.c
+++ src/main.c
@@ -6,12 +6,17 @@
 /*   By: ekeinan <ekeinan@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 13:52:22 by ekeinan           #+#    #+#             */
-/*   Updated: 2025/06/16 13:53:23 by ekeinan          ###   ########.fr       */
+/*   Updated: 2025/06/17 10:22:46 by ekeinan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+/**
+ * 
+ * @returns A pointer to the program's data.
+ * 
+ */
 t_data	*get_data(void)
 {
 	static t_data minirt;
@@ -20,5 +25,5 @@ t_data	*get_data(void)
 
 int	main(int argc, char **argv)
 {
-
+	ft_bzero(get_data(), sizeof(t_data));
 }
