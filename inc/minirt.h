59bf26fa0f43e61e62896b1b984c0ce4d0e5e333
Johnny Varila <jvarila@student.hive.fi>
2025-07-14 16:47:08 +0300

    Moved xcalloc to utils_02.c

--- inc/minirt.h
+++ inc/minirt.h
@@ -534,7 +534,6 @@ void			xadd_intersection(t_ray *ray, t_ray_x_obj intersection);
 void			empty_intersections(t_ray *ray);
 
 // intersections/intersections_01.c
-void			*xcalloc(size_t nmemb, size_t size);
 void			xinit_ray_intersections(t_ray *ray);
 void			xadd_intersection(t_ray *ray, t_ray_x_obj intersection);
 
