9148064b3229f3b22db9126f9c8ff1fbeb55b5a1
Eve Aviv Keinan <evavkein@gmail.com>
2025-08-01 15:30:18 +0300

    controls initial commit

--- inc/settings.h
+++ inc/settings.h
@@ -13,14 +13,73 @@
 #ifndef SETTINGS_H
 # define SETTINGS_H
 
-# ifndef EPSILON
-#  define EPSILON	0.0001
+# include "MLX42.h"
+
+// Movement
+# ifndef KEYBIND_MLEFT
+#  define KEYBIND_MLEFT		MLX_KEY_A
+# endif
+# ifndef KEYBIND_MRIGHT
+#  define KEYBIND_MRIGHT	MLX_KEY_D
+# endif
+# ifndef KEYBIND_MFORWARD
+#  define KEYBIND_MFORWARD	MLX_KEY_W
+# endif
+# ifndef KEYBIND_MBACKWARD
+#  define KEYBIND_MBACKWARD	MLX_KEY_S
+# endif
+# ifndef KEYBIND_MUP
+#  define KEYBIND_MUP		MLX_KEY_R
+# endif
+# ifndef KEYBIND_MDOWN
+#  define KEYBIND_MDOWN		MLX_KEY_F
 # endif
 
+// Rotation
+# ifndef KEYBIND_RX
+#  define KEYBIND_RX		MLX_KEY_X
+# endif
+# ifndef KEYBIND_RY
+#  define KEYBIND_RY		MLX_KEY_Y
+# endif
+# ifndef KEYBIND_RZ
+#  define KEYBIND_RZ		MLX_KEY_Z
+# endif
+// Reverse rotation
+# ifndef KEYBIND_RREV
+#  define KEYBIND_RREV		MLX_KEY_LEFT_SHIFT
+# endif
+
+// Control object
+# ifndef KEYBIND_OBJ
+#  define KEYBIND_OBJ		MLX_KEY_LEFT_CONTROL
+# endif
+
+// Move faster
+# ifndef KEYBIND_FAST
+#  define KEYBIND_FAST		MLX_KEY_LEFT_ALT
+# endif
+# ifndef MOVEMENT_BASE
+#  define MOVEMENT_BASE		1
+# endif
+# ifndef MOVEMENT_MULT
+#  define MOVEMENT_MULT		20
+# endif
+
+// Screenshot
+# ifndef KEYBIND_SAVE
+#  define KEYBIND_SAVE		MLX_KEY_SPACE
+# endif
+
+// Movement
+# ifndef KEYBIND_QUIT
+#  define KEYBIND_QUIT		MLX_KEY_ESCAPE
+# endif
+
+// Resolution
 # ifndef RES_X
 #  define RES_X		1920	// 3840	1920	1280
 # endif
-
 # ifndef RES_Y
 #  define RES_Y		1080	// 2160	1080	720
 # endif
@@ -30,7 +89,11 @@
 # endif
 
 # ifndef TICK
-#  define TICK	500
+#  define TICK		500
+# endif
+
+# ifndef EPSILON
+#  define EPSILON	0.0001
 # endif
 
 #endif
